4b5747fc2215f30bf4812af5d9bdb866122fe8b4
unknown <codeveloper137@gmail.com>
2024-12-17 13:24:00 -0500

    Proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-Homepage
-    </div>
+    <>
+    <BrowserRouter>
+    <Nav/>
+    <Main/>
+    <Menu />
+    <Footer />
+    </BrowserRouter>
+    </>
+
   );
 }
 
